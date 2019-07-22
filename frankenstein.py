cb516e98ca5789c2cbdb6c4d0e26d2f0bce3b3c9
Matthew <matthew.yu007@gmail.com>
2019-07-22 13:43:46 -0700

    randomizes 3 second clips and exports

--- frankenstein.py
+++ frankenstein.py
@@ -1,11 +1,12 @@
 from moviepy.editor import *
+import random
 
 def get_file_paths(folder_path):
     file_name_list = os.listdir(folder_path)
 
     path_name_list = []
     for name in file_name_list:
-        if name == ".DS_Store":
+        if name == ".DS_Store": 
             pass
         else:
             path_name_list.append(folder_path + "/"+ name)
@@ -16,8 +17,10 @@ unattachedVideos = []
 
 for x in listOfVideoPaths: 
     myClip = VideoFileClip(x)
-    unattachedVideos.append(myClip)
+    for y in range(0, int(myClip.duration)-3, 3): 
+        unattachedVideos.append(myClip.subclip(y, y+3))
 
+random.shuffle(unattachedVideos)
 
 finalVideo = concatenate_videoclips(unattachedVideos)
-finalVideo.write_videofile("export.mp4")
+finalVideo.write_videofile("export.mp4", audio="music.mp4")
